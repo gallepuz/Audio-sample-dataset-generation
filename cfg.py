ef4a4d248a5be221bb2d366f9389637019fda8b2
gallepuz <guillemallepuz@gmail.com>
2022-09-08 13:00:39 +0200

    added more settings

--- cfg.py
+++ cfg.py
@@ -2,10 +2,15 @@
 Dataset configuration file.
 """
 
-sr  = 192000            #samplerate
-f   = 997               #frequency 
-l   = 1                 #length    
-fs  = 1                 #features  
-dc  = 0.05              #max dc offset higher and lower limit
-sd  = 123456789         #random gen seed
-snr = 35                #signal to noise ratio
+sr  = 192000                #samplerate
+f   = 997                   #frequency 
+l   = 1                     #length    
+fs  = 1                     #features  
+dc  = 0.05                  #max dc offset higher and lower limit
+sd  = 42                    #random gen seed
+snr = 35                    #signal to noise ratio
+
+bl = 10                     #block of zeros length. Comment or
+#bl = "random"              #uncomment alternatively
+
+bug_name = "BLOCK_ZEROS"    #bug name appended at the end of each sample
